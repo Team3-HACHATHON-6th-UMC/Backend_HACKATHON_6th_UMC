8d02763c7523df44ca8a54fdab9f401967d9e86d
ParkJh38 <harryest@naver.com>
2024-07-05 00:32:19 +0900

    [refactor] annotation 추가

--- src/main/java/umc/hackathon/domain/entity/PlantImage.java
+++ src/main/java/umc/hackathon/domain/entity/PlantImage.java
@@ -1,9 +1,18 @@
 package umc.hackathon.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.hackathon.domain.common.BaseEntity;
 
 @Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class PlantImage extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
