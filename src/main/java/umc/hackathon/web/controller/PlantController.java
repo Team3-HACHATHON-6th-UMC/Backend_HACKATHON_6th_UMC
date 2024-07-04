9b505c928471baa37e39c9b7cd5b3a6b092736a7
[ddhi7] <[tjsanf211323@gmail.com]>
2024-07-04 23:51:02 +0900

    feat/21 식물api

---
+++ src/main/java/umc/hackathon/web/controller/PlantController.java
@@ -0,0 +1,35 @@
+package umc.hackathon.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import umc.hackathon.apiPayload.ApiResponse;
+import umc.hackathon.repository.PlantRepository;
+import umc.hackathon.service.PlantService;
+import umc.hackathon.web.dto.Plant.PlantResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class PlantController {
+
+    private final PlantService plantService;
+    private final PlantRepository plantRepository;
+
+    //식물 전체 조회
+//    @GetMapping("/plants")
+//    public ApiResponse<PlantResponseDTO> getPlant() {
+//        PlantResponseDTO plants = plantService.getPlant();
+//        return ApiResponse.onSuccess(plants);
+//    }
+
+    //식물 상세 조회
+    @GetMapping("/plants/{plantId}")
+    public ApiResponse<PlantResponseDTO> getPlantDetails(@PathVariable Long plantId){
+        PlantResponseDTO plants = plantService.getPlantDetails(plantId);
+        return ApiResponse.onSuccess(plants);
+    }
+
+}
